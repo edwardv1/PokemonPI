973473ae4f75f07681f89d26a31db591108b858f
Edward Vera <edwardismael1@gmail.com>
2023-08-20 14:54:51 -0300

    config variabls de entorno

--- api/index.js
+++ api/index.js
@@ -20,12 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config(); //me permite acceder a la varible PORT en el .env
-const { PORT } = process.env;
+const { PGPORT } = process.env;
 
 // Syncing all the models at once.
 //cada vez que se levanta el server se resetea la BD con el force: true
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('listening at %s', PORT); // eslint-disable-line no-console
+  server.listen(PGPORT, () => {
+    console.log('listening at %s', PGPORT); // eslint-disable-line no-console
   });
 });
