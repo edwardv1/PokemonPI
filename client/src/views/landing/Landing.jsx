7f01fdbdc075614041fc8de8db672ac8d2018d0e
Edward Vera <edwardismael1@gmail.com>
2023-08-25 17:41:58 -0300

    landing responsive 520px

--- client/src/views/landing/Landing.jsx
+++ client/src/views/landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Landing.module.css";
 import { Link } from "react-router-dom";
 import videoPokemon from "../../images/pokemon.mp4";
@@ -6,8 +6,27 @@ import logoPoke from "../../images/logopoke.png";
 
 export default function Landing() {
 
+  //Codigo para controlar el tamaño de pantalla
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div >
+    <div
+    className={`${styles.background} ${
+      windowWidth <= 520 ? styles.backgroundSmall : null
+    }`}
+    >
       <video className={styles.background} loop autoPlay muted>
         <source src={videoPokemon} type="video/mp4"></source>
       </video>
